5d474d0850b7d11b09c5ccac3d92eb6e004bf2c3
Alex Stolzoff <alexstolznut@hotmail.com>
2019-08-23 23:50:17 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -8,5 +8,9 @@ main.addEventListener('click', function(event) {
 
 const input = document.querySelector('input');
 input.addEventListener('keydown', function(e){
+  if (e.key === 'g') {
+    return e.preventDefault();
+  } else {
   console.log(e.key)
+}
 })
