3c9c7394641a8357ae57d7007c7a3d3292187d9e
Alex Stolzoff <alexstolznut@hotmail.com>
2019-08-23 23:39:40 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const main = document.getElementById('main');
+
+main.addEventListener('click', function(){
+  alert('I was clicked!');
+})
